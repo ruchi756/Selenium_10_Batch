ccd0cb304450ec07899d86e79a3815433c681d86
ruchi <ruchi@INBOOK_Y2_PLUS>
2025-02-01 10:54:17 +0530

    Github class Added

---
+++ sampleMaven/src/test/java/gitHub/ToLearnGitHub.java
@@ -0,0 +1,12 @@
+package gitHub;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToLearnGitHub {
+	@Test
+	public void LearnGithub() {
+		Reporter.log("Github class Added",true);
+	}
+
+}
